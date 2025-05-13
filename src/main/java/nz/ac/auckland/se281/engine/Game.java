ca36430263052e28d296daf090cbb393314c5c28
RohitB2005 <rohitvb2005@gmail.com>
2025-05-13 16:18:06 +1200

    added checks and message cli for different error cases in the play command. Set the variables for the round number, player name, etc

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -4,16 +4,40 @@ import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
+
+  private int thisRound;
+  private int totalRounds;
+  private String namePlayer;
+  private boolean gameInProgress;
+
   public static String AI_NAME = "HAL-9000";
 
-  public Game() {}
-
-  public void newGame(Difficulty difficulty, int numRounds, String[] options) {
-    String namePlayer = options[0];
-    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+  public Game() {
+    this.thisRound = 1;
+    this.gameInProgress = false;
   }
 
-  public void play() {}
+  public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    this.namePlayer = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(this.namePlayer);
+    this.totalRounds = numRounds;
+    this.thisRound = 1;
+    this.gameInProgress = true;
+  }
+
+  public void play() {
+
+    if (!gameInProgress) {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+      return;
+    }
+
+    if (thisRound > totalRounds) {
+      MessageCli.PRINT_END_GAME.printMessage();
+    }
+
+    thisRound++;
+  }
 
   public void showStats() {}
 }
